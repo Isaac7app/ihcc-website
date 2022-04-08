f303274f093f95104c9d64bf8aefacf791e8a290
Isaac7app <ik.appiagyei@gmail.com>
2022-04-08 12:22:19 +0200

    added functionalities to the card components and also css

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function CardItem() {
+function CardItem(props) {
   return (
     <>
-      <li className="card__item">
-        <Link className="cards__item__link">
-          <figure className="cards__item__pic-wrap">
-            <img src="/" alt="IHCC pic" className="cards__item__img" />
+      <li className="cards__item">
+        <Link className="cards__item__link" to={props.path}>
+          <figure className="cards__item__pic-wrap" data-category={props.label}>
+            <img
+              className="cards__item__img"
+              alt="IHCC pictures"
+              src={props.src}
+            />
           </figure>
-          <div className="card__item__info">
-            <h5 className="cards__item__text" />
+          <div className="cards__item__info">
+            <h5 className="cards__item__text">{props.text}</h5>
           </div>
         </Link>
       </li>
