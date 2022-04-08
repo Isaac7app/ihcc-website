6d81b720fd0f5cfd72331130eb59da24bac2f405
Isaac7app <ik.appiagyei@gmail.com>
2022-04-08 11:57:26 +0200

    adding code to the cardItem component

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -2,7 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function CardItem() {
-  return <></>;
+  return (
+    <>
+      <li className="card__item">
+        <Link className="cards__item__link">
+          <figure className="cards__item__pic-wrap">
+            <img src="/" alt="IHCC pic" className="cards__item__img" />
+          </figure>
+          <div className="card__item__info">
+            <h5 className="cards__item__text" />
+          </div>
+        </Link>
+      </li>
+    </>
+  );
 }
 
 export default CardItem;
