525e789ffdc250413c5aa46e82c57accb70a3bf7
Isaac7app <ik.appiagyei@gmail.com>
2022-04-08 10:00:19 +0200

    added the herosection and also wrote some css with it

---
+++ src/components/HeroSection.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Button } from "./Button";
+import "../App.css";
+import "./HeroSection.css";
+
+function HeroSection() {
+  return (
+    <div className="hero-container">
+      <video src="/videos/video-2mp4" autoPlay loop muted />
+      <h1>COME WORSHIP WITH US</h1>
+      <p>What are you waiting for?</p>
+      <div className="hero-btns">
+        <Button
+          className="btn"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          GET TO KNOW GOD
+        </Button>
+        <Button
+          className="btn"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+        >
+          WATCH US <i className="far fa-play-circle" />
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,7 +28,7 @@ function Navbar() {
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
             IHCC <i className="fa-solid fa-church" />
           </Link>
           <div className="menu-icon" onClick={handleClick}>
