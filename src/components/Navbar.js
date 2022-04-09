eeb6ad61e5f741c607a9d38806bce87692bb1641
Isaac7app <ik.appiagyei@gmail.com>
2022-04-09 12:45:12 +0200

    linked various routes to their various destinations

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -51,7 +51,7 @@ function Navbar() {
             </li>
             <li className="nav-item">
               <Link
-                to="/our ministries"
+                to="/products"
                 className="nav-links"
                 onClick={closeMobileMenu}
               >
