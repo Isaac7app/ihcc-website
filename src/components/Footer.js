a7827fbdb0480827877e092bb764f318232cdf95
Isaac7app <ik.appiagyei@gmail.com>
2022-04-11 13:19:32 +0200

    adding the footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <section className="footer-subscription">
+        <p className="footer-subcription-heading">
+          Please Leave Us Your Info So We Can Reach Out To You. Thanks!
+        </p>
+        <p className="footer-subscription-text">Jesus Loves You</p>
+      </section>
+    </div>
+  );
+}
+
+export default Footer;
