fdf90ce83b91540264fd28fd142f3fd06c625a0e
Isaac7app <ik.appiagyei@gmail.com>
2022-04-14 10:43:00 +0200

    changed sign up page

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -2,5 +2,9 @@ import React from "react";
 import "../../App.css";
 
 export default function SignUp() {
-  return <h1 className="sign-up">Sign Up</h1>;
+  return (
+    <h1 className="sign-up" style={{ color: "lightgreen" }}>
+      SignUp
+    </h1>
+  );
 }
